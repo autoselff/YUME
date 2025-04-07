716dc4d62561d18799665b21c583717fb08a8a2c
dazai <60112867+dazaizer0@users.noreply.github.com>
2025-04-07 13:56:34 +0200

    first steps

---
+++ YUME/src/engine/core/core.hpp
@@ -0,0 +1,105 @@
+#ifndef YUME_CORE_HPP
+#define YUME_CORE_HPP
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <stb/stb_image.h>
+#include <iostream>
+
+void mouseCallback(GLFWwindow* window, double xpos, double ypos);
+
+namespace yume {
+    unsigned int WINDOW_WIDTH{ 1280 };
+    unsigned int WINDOW_HEIGHT{ 720 };
+
+    GLFWwindow* _window;
+
+    float delta_time{};
+    float last_frame{};
+
+    void setWindowSize(unsigned int width, unsigned int height) {
+        WINDOW_WIDTH = width;
+        WINDOW_HEIGHT = height;
+    }
+
+    void init(const std::string& title) {
+        // WINDOW INITIALIZATION
+        if (!glfwInit()) {
+            std::cerr << "Failed to initialize GLFW" << std::endl;
+            return;
+        }
+
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
+
+        _window = glfwCreateWindow((int)WINDOW_WIDTH, (int)WINDOW_HEIGHT, title.c_str(), nullptr, nullptr);
+
+        if (_window == nullptr) {
+            std::cerr << "Failed to create GLFW window" << std::endl;
+            glfwTerminate();
+            return;
+        }
+        else {
+            std::cout << "GLFW window created correctly" << std::endl;
+        }
+
+        glfwMakeContextCurrent(_window);
+
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+            std::cerr << "Failed to initialize GLAD\n";
+            return;
+        }
+        else {
+            std::cout << "GLAD initialized correctly\n> yumegl2 0.0.1 <\n";
+        }
+
+        glfwSwapInterval(1);
+        std::cout << std::flush;
+    }
+
+    void update() {
+        auto current_frame = static_cast<float>(glfwGetTime());
+        delta_time = current_frame - last_frame;
+        last_frame = current_frame;
+    }
+
+    GLFWwindow* getWindowPointer() {
+        return _window;
+    }
+
+    bool isWindowOpen() {
+        return !glfwWindowShouldClose(_window);
+    }
+
+    void setWindowStatus(bool value) {
+        glfwSetWindowShouldClose(_window, !value);
+    }
+
+    void swapBuffersPollEvents() {
+        glfwSwapBuffers(_window);
+        glfwPollEvents();
+    }
+
+    void setCursorPosCallback(GLFWcursorposfun func) {
+        glfwSetCursorPosCallback(_window, func);
+    }
+
+    namespace eExit { // ENGINE'S PROCESS TERMINATION FUNCTIONS
+        void close() {
+            std::cout << "Opengl has been successfully closed" << std::endl;
+
+            glfwTerminate();
+        }
+
+        // void delete...(...) { ... }
+    }
+}
+
+#endif
--- YUME/src/main.cpp
+++ YUME/src/main.cpp
@@ -1,51 +1,11 @@
-﻿#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+﻿#include "engine/core/core.hpp"
 
-#include <iostream>
+int main() {
+    yume::init("YUME");
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-
-const unsigned int SCREEN_WIDTH = 800;
-const unsigned int SCREEN_HEIGHT = 600;
-
-int main()
-{
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "YUME", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
+    while(yume::isWindowOpen()) {
+        yume::update();
     }
 
-    while (!glfwWindowShouldClose(window))
-    {
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
-    return 0;
+    yume::eExit::close();
 }
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
-
