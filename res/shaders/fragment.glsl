ba36d0ce52266971345ba3387f201b4570098e9b
dazai <j.szymanski0100@gmail.com>
2025-08-17 10:00:52 +0200

    TexSquare - FIX (Color) | NEXT - Game Scenes

--- res/shaders/fragment.glsl
+++ res/shaders/fragment.glsl
@@ -1,14 +1,13 @@
 #version 460 core
 
-out vec4 FragColor;
-
 in vec3 ourColor;
 in vec2 TexCoord;
+out vec4 FragColor;
 
-uniform sampler2D ourTexture;
+uniform sampler2D texture1;
 
 void main()
 {
-    //FragColor = texture(ourTexture, TexCoord) * vec4(ourColor, 1.0);  
-    FragColor = texture(ourTexture, TexCoord);
+    vec4 texColor = texture(texture1, TexCoord);
+    FragColor = texColor * vec4(ourColor, 1.0);
 }
